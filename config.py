e41ae516ef108476c385a0f5ef1e4d463ab6fb57
Kim2002aa <154697088+Kim2002aa@users.noreply.github.com>
2023-12-26 06:09:39 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+bot_token = "6803695045:AAFzcPnpVCn0Pghcnk_brGuClKs0rSOPU5g"
+
+admins = [6056917058]
+
+BOT_NICKNAME = "" #логин бота без собачки
+
+not_sub_message = "Вы должны подписаться на все каналы"
